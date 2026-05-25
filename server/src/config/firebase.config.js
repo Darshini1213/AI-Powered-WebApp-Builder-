b916ca11790fee7a1a75142179773fcc4f0bdf76
Venkata Darshini <venkatadarshini@gmail.com>
2026-05-25 12:34:55 +0530

    Update Firebase config to use environment variable for production

--- server/src/config/firebase.config.js
+++ server/src/config/firebase.config.js
@@ -1,9 +1,15 @@
 import admin from "firebase-admin";
 import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
-);
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+} else {
+  serviceAccount = JSON.parse(
+    fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
+  );
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
